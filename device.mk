0ab41638a99e2ebcfa71f85fff0916e1b2aaef57
Kevin F. Haggerty <haggertk@lineageos.org>
2020-04-08 19:22:18 -0600

    gtelwifiue: Set shipping API level in the Lineage product makefile
    Change-Id: I1dc0862a93b3ee2da6673741899f295129f5297c

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@ $(call inherit-product, device/samsung/gte-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/gtelwifiue
 
+# Shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
+
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
