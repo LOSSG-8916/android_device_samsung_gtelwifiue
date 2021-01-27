9344635255d763154ecd84e0a5935f78b2a2c094
zeeshan <itexpert120@gmail.com>
2021-01-27 20:20:23 +0500

    new lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := device/samsung/gtelwifiue
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/rr_gtelwifiue.mk \
-	$(LOCAL_DIR)/pa_gtelwifiue.mk \
 	$(LOCAL_DIR)/lineage_gtelwifiue.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_gtelwifiue-eng \
+    lineage_gtelwifiue-userdebug \
+    lineage_gtelwifiue-user
