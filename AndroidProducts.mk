d325596fd4d476712974b6cb7df4d879f99a7778
palmbeach05 <davmcleod87@aol.com>
2021-05-11 22:27:34 -0400

    Remove vendorsetup and add lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,3 +3,8 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_gtelwifiue.mk \
 	$(LOCAL_DIR)/pa_gtelwifiue.mk \
 	$(LOCAL_DIR)/lineage_gtelwifiue.mk
+	
+COMMON_LUNCH_CHOICES := \
+    lineage_gtelwifiue-eng \
+    lineage_gtelwifiue-userdebug \
+    lineage_gtelwifiue-user
