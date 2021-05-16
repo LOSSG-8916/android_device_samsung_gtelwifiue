7e1f2223db198a13bd3a7efab0720ca85c97ea21
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2021-05-15 21:35:17 -0400

    gtelwifiue : Add Android Go defaults
    - Add Android Go defaults
    - Add Android Go building optional
    - Removing RR & PA .mk files

--- lineage_gtelwifiue.mk
+++ lineage_gtelwifiue.mk
@@ -2,6 +2,11 @@
 # Inherit from common
 $(call inherit-product, device/samsung/gte-common/lineage.mk)
 
+ifeq ($(PRODUCT_TYPE),go)
+    # Inherit common Android Go configurations
+    $(call inherit-product, build/target/product/go_defaults.mk)
+endif
+
 $(call inherit-product, device/samsung/gtelwifiue/device.mk)
 
 ## Device identifier. This must come after all inclusions
