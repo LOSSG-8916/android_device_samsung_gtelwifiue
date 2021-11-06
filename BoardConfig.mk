70630c5af734c6000e13e1ac5f9de7eb967d3adb
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2021-11-06 10:57:51 -0400

    gtelwifiue : Add bdroid_buildcfg.h
    Adding bdroid_buildcfg.h to this device allow me to change the same to batter match the device

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ TARGET_OTA_ASSERT_DEVICE := gtelwifiue,samsung_sm_t560nu,t560nu
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_gtelwifi_usa_defconfig
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+
 # Display
 TARGET_SCREEN_DENSITY := 160
 
