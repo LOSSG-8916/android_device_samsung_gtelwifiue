6e2ed0008ee4a750f41020ce969355987cf20e0c
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2023-06-27 14:28:34 -0400

    gtesqltespr: Update shipping API level form 19 to 22

--- device.mk
+++ device.mk
@@ -7,7 +7,7 @@ $(call inherit-product, device/samsung/gte-common/device-common.mk)
 LOCAL_PATH := device/samsung/gtelwifiue
 
 # Shipping API level
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
 
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
